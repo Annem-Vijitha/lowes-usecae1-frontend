abbc4af274e0dbf8364e7d52c520bbfda14bbfc4
NewaleVaishali <99384467+NewaleVaishali@users.noreply.github.com>
2023-06-11 00:01:32 +0530

    Update connector.js

--- src/config/connector.js
+++ src/config/connector.js
@@ -10,11 +10,12 @@ import userContext from "./context";
 import Reducer from "./reducer";
 
 // type tags
+
 import {
   GET_CATEGORIES,
   GET_ITEMS,
   POST_CATEGORY,
-  POST_ITEMS
+  POST_ITEMS,
 } from "./values";
 
 const Context = props => {
@@ -32,22 +33,25 @@ const Context = props => {
 
   // POST Methods
   const addCategory = async category => {
-    const res = await axiosClient.post("/categories/add", category);
+    const res = await axiosClient.post("/categories/add", category)
 
     dispatch({
       type: POST_CATEGORY,
       payload: res.data
     });
   };
-  const addItem = async item => {
-    const res = await axiosClient.post("/items/add", item);
-
-    dispatch({
-      type: POST_ITEMS,
-      payload: res.data
-    });
+  const addItem = async (item) => {
+    try {
+      const res = await axiosClient.post("/items/add", item);
+      dispatch({
+        type: POST_ITEMS,
+        payload: res.data,
+      });
+    } catch (error) {
+      console.error("Error adding item:", error);
+      // Handle error scenario if needed
+    }
   };
-
   // GET Methods
 
   const getCategories = async () => {
@@ -66,6 +70,60 @@ const Context = props => {
       payload: res.data
     });
   };
+  const handleDelete = async categoryID => {
+   
+    axiosClient
+      .delete(`/categories/delete/${categoryID}`) // replace with your backend API endpoint
+      .then((response) => {
+        console.log(response.data); // handle success response
+        // Perform any additional actions after successful deletion
+      })
+      .catch((error) => {
+        console.error(error); // handle error response
+        // Perform any additional error handling
+      });
+  };
+  const handleDelete1 = async itemID => {
+   
+    axiosClient
+      .delete(`/items/delete/${itemID}`) // replace with your backend API endpoint
+      .then((response) => {
+        console.log(response.data); // handle success response
+        // Perform any additional actions after successful deletion
+      })
+      .catch((error) => {
+        console.error(error); // handle error response
+        // Perform any additional error handling
+      });
+  };
+
+  const updateCategory = async(category,id) => {
+   console.log("id:",id);
+    axiosClient
+      .put(`/categories/update/${id}`,category) // replace with your backend API endpoint
+      .then((response) => {
+        console.log(response.data); // handle success response
+        // Perform any additional actions after successful deletion
+      })
+      .catch((error) => {
+        console.error(error); // handle error response
+        // Perform any additional error handling
+      });
+  };
+  const updateItem = async(item,id) => {
+   console.log("id:",id);
+    axiosClient
+      .put(`/items/update/${id}`,item) // replace with your backend API endpoint
+      .then((response) => {
+        console.log(response.data); // handle success response
+        // Perform any additional actions after successful deletion
+      })
+      .catch((error) => {
+        console.error(error); // handle error response
+        // Perform any additional error handling
+      });
+  };
+
 
   return (
     <userContext.Provider
@@ -75,13 +133,15 @@ const Context = props => {
         addCategory,
         addItem,
         getCategories,
-        getItems
+        getItems,
+        handleDelete,
+        handleDelete1,
+        updateCategory,
+        updateItem
       }}
     >
       {props.children}
     </userContext.Provider>
   );
     };
-
-
 export default Context;
